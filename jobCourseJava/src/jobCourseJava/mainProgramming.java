9855626fcbf479089ffb990297601ea73855caee
CÉLIO MENDES PEREIRA <78619443+celiokenosis@users.noreply.github.com>
2022-09-30 15:44:23 -0300

    Add files via upload

---
+++ jobCourseJava/src/jobCourseJava/mainProgramming.java
@@ -0,0 +1,11 @@
+package jobCourseJava;
+
+public class mainProgramming {
+
+	public static void main(String[] args) {
+		
+		int numero = 10;
+		
+		System.out.println("Meu número: " + numero);
+	}
+}
